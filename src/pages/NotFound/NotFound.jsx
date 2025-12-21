0314af309cc9912320738841b425967ac105af25
fzansari007 <49395275+fzansari007@users.noreply.github.com>
2025-12-21 18:08:48 +0530

    component updated

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import "./notFound.css";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="notfound_wrapper">
+      <div className="notfound_card">
+        <div className="notfound_code">404</div>
+
+        <h1>Page Not Found</h1>
+        <p>
+          Oops! The page you’re looking for doesn’t exist or may have been
+          moved.
+        </p>
+
+        <button onClick={() => navigate("/")}>Go Back Home</button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
